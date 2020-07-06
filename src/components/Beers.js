d8fcea57e052e414a2d1c19ac7700b85426c4717
rltrtl <rickytoledo@LAPTOP-3UVQIICU.localdomain>
2020-07-06 09:16:12 -0700

    finished lab

---
+++ src/components/Beers.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class Beers extends Component {
+  state = {
+    beers: [],
+  };
+
+  componentDidMount() {
+    axios.get('https://ih-beers-api2.herokuapp.com/beers').then((res) => {
+      this.setState({
+        beers: res.data,
+      });
+    });
+  }
+
+  showTheBeers() {
+    return this.state.beers.map((eachBeer, i) => {
+      return (
+        <Link key={i} to={'/beers/' + eachBeer._id}>
+          <li>{eachBeer.name}</li>
+        </Link>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <Link to="/">Home</Link>
+        </header>
+        <ul>{this.showTheBeers()}</ul>
+      </div>
+    );
+  }
+}
+
+export default Beers;
